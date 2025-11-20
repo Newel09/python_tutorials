e6f902c8c622c7c26caea901f65433f9afd5bd44
newel <tsogbenewel@gmail.com>
2025-11-20 07:32:56 +0000

    even or even_or_odd

--- functions2.py
+++ functions2.py
@@ -1,17 +1,8 @@
-"""
-def calculate_area(length: float, width: float) -> float:
-   
-   return length * width
-
-area = calculate_area(5.0, 3.0)
-
-print(f"The area is {area}")
-"""
 
 def x(y: int, z: int) -> int:
-    return ((y/z) / 10 * z)
+    return ((y/z) / 10 * z) # Defines formula with the variables
 
-modulos = x(10,3)
+modulos = x(10,3) # Gives values to the variables in the function
 
 temp = x(20, 2)
 print(modulos)
