0b791df6bf99704220c015fa55205dc3556a71a3
newel <tsogbenewel@gmail.com>
2025-11-20 02:20:11 +0000

    functions and nested functions

--- all_files/functions_area.py
+++ all_files/functions_area.py
@@ -1,23 +1,12 @@
-"""def greet(name: str) -> None:
-    Print a greeting.
-
-    Args:
-        name: Person to greet.
-    
-    print(f"Hello, {name}!")
-    
-    """
-
 
 def calculate_area(length: float, width: float) -> float:
    
-   return length * width
+   return length * width    # Defines the formular for calculating area of a rectangle
 
 area = calculate_area(5.0, 3.0)
 
 print(f"The area is {area}")
 
 
-   
 
 
