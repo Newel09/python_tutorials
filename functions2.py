1bd403cfd83aeed95bd5b1d87096afc0c0762810
newel <tsogbenewel@gmail.com>
2025-11-20 13:22:05 +0000

    volume

--- functions2.py
+++
@@ -1,9 +0,0 @@
-
-def x(y: int, z: int) -> int:
-    return ((y/z) / 10 * z) # Defines formula with the variables
-
-modulos = x(10,3) # Gives values to the variables in the function
-
-temp = x(20, 2)
-print(modulos)
-print(temp)
