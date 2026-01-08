f38883b3ee4be5b5f8d27ce61ca8f7046ab13dd8
newel <tsogbenewel@gmail.com>
2026-01-08 21:40:47 +0000

    django projects

---
+++ all_files/intro_to_django/shop_api/products/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    category = models.CharField(max_length=50)
+    stock = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+    class Meta:
+        verbose_name = "Product"
+        verbose_name_plural = "Products"
+
